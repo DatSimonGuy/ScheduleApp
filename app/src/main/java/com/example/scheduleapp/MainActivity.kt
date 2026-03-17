006221dc096f678e3c212c8f8ef89f76c60bd22b
DatSimonGuy <62205459+Lazerpl12@users.noreply.github.com>
2026-03-17 01:08:29 +0100

    Replace all mentions of DSBApp with ScheduleApp

--- app/src/main/java/com/example/scheduleapp/MainActivity.kt
+++ app/src/main/java/com/example/scheduleapp/MainActivity.kt
@@ -4,19 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.scheduleapp.elements.navigation.Navbar
-import com.example.scheduleapp.ui.theme.DSBAppTheme
+import com.example.scheduleapp.ui.theme.ScheduleAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DSBAppTheme {
+            ScheduleAppTheme {
                 Navbar()
             }
         }
--- app/src/main/java/com/example/scheduleapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/scheduleapp/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun DSBAppTheme(
+fun ScheduleAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
